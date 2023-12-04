66f7d6fbbcf7d58b57659e0911230382d66aab46
siyuan <zhaosiyuan1098@qq.com>
2023-12-04 21:24:50 +0800

    添加transform_matrix_accuracy_test函数，相机坐标系下验证转移矩阵正确

--- src/transform_matrix_calculator.py
+++ src/transform_matrix_calculator.py
@@ -9,9 +9,10 @@ CAMERA_RESOLUTION_Y = 720
 CAMERA_FRAME_RATE=30
 
 SHOW_IMAGE=False
-NEEDLE_MARKER_LENGTH=0.008
-REF_MARKER_LENGTH=0.045
-NEEDLE_LENGTH=0.009
+NEEDLE_MARKER_LENGTH=0.004
+REF_MARKER_LENGTH=0.008
+NEEDLE_LENGTH=0.012185
+MARKER_TO_NEEDLE_DEPTH=0.001082-0.000202/2
 
 
 class TransformMatrixCalculator():
@@ -100,18 +101,18 @@ class TransformMatrixCalculator():
                              
                 #使用transform_matrix计算marker和相机的距离
                 distance = np.sqrt(np.sum(np.square(tvec[0])))
-                print("distance:",distance)
+                #print("distance:",distance)
                 
                 
                 if aruco_dict.markerSize == 4:
                     self.transform_matrix_4 = transform_matrix
-                    print("transform_matrix_4:",transform_matrix)
+                    # print("transform_matrix_4:",transform_matrix)
                     
                 if aruco_dict.markerSize == 5:
                     self.transform_matrix_5 = transform_matrix
-                    print("transform_matrix_5:",transform_matrix)
-                else:
-                    return
+                    #print("transform_matrix_5:",transform_matrix)
+                # else:
+                #     return
                 return transform_matrix
             else:
                 print("no change")
@@ -120,7 +121,7 @@ class TransformMatrixCalculator():
             pass
         return None
         
-    def get_transformed_needle_points(self,aruco_dict):
+    def get_transformed_needle_points(self,aruco_dict,start_point=np.array([0, 0, -0.001, 1]),end_point = np.array([0, NEEDLE_LENGTH, -0.001, 1])):
         """
         Calculates the transformed needle points based on the transformation matrix.
 
@@ -130,17 +131,16 @@ class TransformMatrixCalculator():
         Returns:
             None
         """
-        if self.transform_matrix_4 is None and self.transform_matrix_5 is None:
-            return None, None
-        if aruco_dict.markerSize == 4:
+   
+        if self.transform_matrix_4 is not None and aruco_dict.markerSize == 4:
             transformation_matrix = self.transform_matrix_4
-            
-        if aruco_dict.markerSize == 5:
+        elif self.transform_matrix_5 is not None and aruco_dict.markerSize == 5:
             transformation_matrix = self.transform_matrix_5
+        else:
+            return None, None,None,None
         
-        # Define the start point and direction of the line segment
-        start_point = np.array([0, 0, -0.001, 1])  # 1 cm along -z
-        end_point = np.array([0, NEEDLE_LENGTH, -0.001, 1])  # 20 cm along -y
+
+        
         # Transform the points to the camera frame
         start_point_cam = np.dot(transformation_matrix, start_point)
         end_point_cam = np.dot(transformation_matrix, end_point)
@@ -154,7 +154,7 @@ class TransformMatrixCalculator():
         start_point_img = start_point_img[:2]
         end_point_img = end_point_img[:2]
         
-        #强制将start_point_img和end_point_img转换为非负数
+        #强制将start_point_img和end_point_img转换为整数
         start_point_img = start_point_img.astype(np.int32)
         end_point_img = end_point_img.astype(np.int32)
         
@@ -166,21 +166,41 @@ class TransformMatrixCalculator():
         if (self.start_point_img != start_point_img).any() or (self.end_point_img != end_point_img).any():
             self.start_point_img = start_point_img
             self.end_point_img = end_point_img
-            print("start_point_img:",start_point_img,"end_point_img:",end_point_img)
         
-        return start_point_img, end_point_img
-        
-        
-if __name__ == "__main__":
+        return start_point_img, end_point_img,start_point_cam[:3],end_point_cam[:3]
+    
+def transform_matrix_accuracy_test():
     aruco_dict_4 = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
     aruco_dict_5 = aruco.getPredefinedDictionary(aruco.DICT_5X5_50)
     transform_matrix_calculator = TransformMatrixCalculator()
     while(1):
         start = time.time()
-        # transform_matrix_calculator.calculate_transform_matrix(aruco_dict_4,REF_MARKER_LENGTH)
-        trasform_matrix=transform_matrix_calculator.calculate_transform_matrix(aruco_dict_5,NEEDLE_MARKER_LENGTH)
-        start_point, end_point = transform_matrix_calculator.get_transformed_needle_points(aruco_dict_5)
-        end = time.time()
+        transform_matrix_ref=transform_matrix_calculator.calculate_transform_matrix(aruco_dict_5,REF_MARKER_LENGTH)
+        transform_matrix_needle=transform_matrix_calculator.calculate_transform_matrix(aruco_dict_4,NEEDLE_MARKER_LENGTH)
+        needle_start_point_3d=np.array([0, 0, -MARKER_TO_NEEDLE_DEPTH, 1])
+        needle_end_point_3d=np.array([0, NEEDLE_LENGTH, -MARKER_TO_NEEDLE_DEPTH, 1])
+        needle_start_point_2d, needle_end_point_2d,_,_ = transform_matrix_calculator.get_transformed_needle_points(aruco_dict_4,needle_start_point_3d,needle_end_point_3d)
+        end=time.time()
+        _,needle_end_img_2d,_,needle_end_cam_3d = transform_matrix_calculator.get_transformed_needle_points(aruco_dict_4,start_point=np.array([0, 0, -0.001, 1]),end_point = np.array([0, NEEDLE_LENGTH, -0.001, 1]))
+        print("needle_end_img_2d:",needle_end_img_2d,"needle_end_cam_3d:",needle_end_cam_3d)
+        marker_start_point=np.array([REF_MARKER_LENGTH/2, REF_MARKER_LENGTH/2, 0, 1])
+        marker_end_point=np.array([REF_MARKER_LENGTH/2, REF_MARKER_LENGTH/2, 0, 1])
+        _,marker_end_img_2d,_,marker_end_cam_3d=transform_matrix_calculator.get_transformed_needle_points(aruco_dict_5,start_point=marker_start_point,end_point=marker_end_point)
+        print("marker_end_img_2d:",marker_end_img_2d,"marker_end_cam_3d:",marker_end_cam_3d)
+    return None
+def main():
+    aruco_dict_4 = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    aruco_dict_5 = aruco.getPredefinedDictionary(aruco.DICT_5X5_50)
+    transform_matrix_calculator = TransformMatrixCalculator()
+    while(1):
+        start = time.time()
+        transform_matrix_ref=transform_matrix_calculator.calculate_transform_matrix(aruco_dict_5,REF_MARKER_LENGTH)
+        print("transform_matrix_ref:",transform_matrix_ref)
+        transform_matrix_needle=transform_matrix_calculator.calculate_transform_matrix(aruco_dict_4,NEEDLE_MARKER_LENGTH)
+        needle_start_point_3d=np.array([0, 0, -MARKER_TO_NEEDLE_DEPTH, 1])
+        needle_end_point_3d=np.array([0, NEEDLE_LENGTH, -MARKER_TO_NEEDLE_DEPTH, 1])
+        needle_start_point_2d, needle_end_point_2d,_,_ = transform_matrix_calculator.get_transformed_needle_points(aruco_dict_4,needle_start_point_3d,needle_end_point_3d)
+        end=time.time()
         fps=int(1/(end-start))
         if SHOW_IMAGE:
             rgb, depth, depth_8bit, intr_matrix, intr_coeffs = transform_matrix_calculator.get_aligned_images()
@@ -189,18 +209,25 @@ if __name__ == "__main__":
             aruco.drawDetectedMarkers(rgb, corners)
             cv2.putText(rgb, "FPS: {:.0f}".format(fps), (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-            if trasform_matrix is not None:
-                cv2.putText(rgb, "distance: {:.5f}".format(np.sqrt(np.sum(np.square(trasform_matrix[:3,3])))), (10, 60),
+            if transform_matrix_needle is not None:
+                cv2.putText(rgb, "distance: {:.5f}".format(np.sqrt(np.sum(np.square(transform_matrix_needle[:3,3])))), (10, 60),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                if start_point is not None and end_point is not None:
-                    cv2.line(rgb, start_point, end_point, (255, 255, 255), 2)
-                    cv2.putText(rgb, "start_point: {:.0f},{:.0f}".format(start_point[0],start_point[1]), (10, 90),
+                if needle_start_point_2d is not None and needle_end_point_2d is not None:
+                    cv2.line(rgb, needle_start_point_2d, needle_end_point_2d, (255, 255, 255), 2)
+                    cv2.putText(rgb, "start_point: {:.2f},{:.2f}".format(needle_start_point_2d[0],needle_start_point_2d[1]), (10, 90),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                    cv2.putText(rgb, "end_point: {:.0f},{:.0f}".format(end_point[0],end_point[1]), (10, 120),
+                    cv2.putText(rgb, "end_point: {:.2f},{:.2f}".format(needle_end_point_2d[0],needle_end_point_2d[1]), (10, 120),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             
             cv2.imshow('rgb',rgb)
             cv2.waitKey(1)
+        
+        
+if __name__ == "__main__":
+    transform_matrix_accuracy_test()
+    
+    # main()
+    
         
         
 
