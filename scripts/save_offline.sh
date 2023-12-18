2957b804591684ffed69683727874eb932a59622
siyuan <zhaosiyuan1098@qq.com>
2023-12-18 20:37:51 +0800

    add dataset

--- scripts/save_offline.sh
+++ scripts/save_offline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #循环执行 gh_filter.py 10 次,并附带参数i
-for i in {1..10}
+for i in {1..25}
 do
-    python3 ../filters/gh_filter.py $i
+    python3 ./src/offline_data_collector.py $i
 done
--- src/offline_data_collector.py
+++ src/offline_data_collector.py
@@ -8,6 +8,7 @@ CAMERA_RESOLUTION_X = 1280
 CAMERA_RESOLUTION_Y = 720
 CAMERA_FRAME_RATE=30
 RANGE=1000
+MARKER_LENGTH=0.0039
 
 def main():
 
@@ -20,26 +21,19 @@ def main():
 
     for i in range(RANGE):
         start_timne_array[i]=time.time()
-        transform_matrix_array[i] = transform_matrix_calculator.calculate_transform_matrix(aruco_dict_5, CAMERA_RESOLUTION_X, CAMERA_RESOLUTION_Y, CAMERA_FRAME_RATE)
+        transform_matrix_array[i] = transform_matrix_calculator.calculate_transform_matrix(aruco_dict_5,MARKER_LENGTH)
+        print(transform_matrix_array[i])
         end_time_array[i]=time.time()
         
-    save_path='../data/transform_matrix_'+str(sys.argv[1])+'.npy'
+    save_path='./data/transform_matrix_'+str(sys.argv[1])+'.npy'
     np.save(save_path, transform_matrix_array)
-    save_path='../data/start_time_'+str(sys.argv[1])+'.npy'
+    save_path='./data/start_time_'+str(sys.argv[1])+'.npy'
     np.save(save_path, start_timne_array)
-    save_path='../data/end_time_'+str(sys.argv[1])+'.npy'
+    save_path='./data/end_time_'+str(sys.argv[1])+'.npy'
     np.save(save_path, end_time_array)
     
-def test():
-    transform_matrix_array = np.zeros((RANGE,4,4))
-    start_timne_array=np.zeros((RANGE,1))
-    end_time_array=np.zeros((RANGE,1))
     
-    transform_matrix_array[1]=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[0,0,0,1]])
-    print(transform_matrix_array[1])
-    
-    
-test()
+main( )
     
 
 
