b59ee0f66c3430343c6e88b9323b64a1db7663ea
siyuan <1098198414@qq.com>
2023-12-19 17:10:48 +0800

    init ghfilter and generate result figures

--- filter/gh_filter.py
+++ filter/gh_filter.py
@@ -9,22 +9,8 @@ def generate_transform_matrix():
     matrix = np.zeros((4, 4))
     return matrix
 
-def save_transform_matrix(matrix, path):
-    np.save(path, matrix)
-    
 
-def load_transform_matrix(path):
-    matrix = np.load(path)
-    return matrix
 
-def g_h_filter_predict(quaternion, velocity, dt):
-    quaternion_hat = quaternion + velocity * dt
-    return quaternion_hat
-
-def g_h_filter_update(quanternion_hat,measured_quaternion, measured_velocity, dt, g, h):
-    residual = measured_quaternion - quanternion_hat
-    
-    return
 
 def tranform_matrix2quaternion(transform_matrix):
     # 将旋转平移矩阵转换为四元数
@@ -46,9 +32,9 @@ def quaternion2transform_matrix(quaternion):
     return rotation_matrix
 
 
-class gh_filter():
+class GHFilter():
     def __init__(self,g,h,x0,dt=1) -> None:
-        self.velocity=np.zeros((x0.size[0],1))
+        self.velocity=np.zeros(x0.shape[0])
         self.g=g
         self.h=h
         self.dt=dt
@@ -62,9 +48,12 @@ class gh_filter():
         return x_updated
 
 def main():
-    collect_data_path = './data/transform_matrix_1.npy'
+    collect_data_path = './data/transform_matrix_8.npy'
     transform_matrix_array=np.load(collect_data_path)
     quaternionarray=transform_matrixarray2quaternionarray(transform_matrix_array)
+    gh_filter=GHFilter(0.1,0.1,quaternionarray[6,:])
+    for i in range(1000):
+        pass
     return
 
 main()
